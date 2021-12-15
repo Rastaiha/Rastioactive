e726f835fb201bd342328710f29555aec21e00f2
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-12-16 01:55:42 +0330

    complete room0

--- src/containers/Room0/Objects.js
+++ src/containers/Room0/Objects.js
@@ -2,6 +2,7 @@ import { toast } from 'react-toastify';
 
 let isTvClicked = false;
 let isSoundPlying = false;
+let isFakeDoorKnocked = false;
 
 const getAnotherElementById = (element, id) => {
   return element.parent.children.filter((element) => element.attrs.id == id)[0]
@@ -19,13 +20,12 @@ const showElementSoftly = (element, duration) => {
 }
 
 function playAudio(url) {
-  if (isSoundPlying) return
+  if (isSoundPlying || !isTvClicked) return
   isSoundPlying = true;
   const audio = new Audio(url)
   audio.addEventListener('ended', () => { isSoundPlying = false; })
   audio.play();
 }
-
 
 const Objects = [
   {
@@ -46,7 +46,7 @@ const Objects = [
   },
   {
     imageUrl: process.env.PUBLIC_URL + '/Room0/tv.png',
-    id: "121",
+    id: "tv",
     x: 1930,
     y: 575,
     isHover: false,
@@ -56,15 +56,85 @@ const Objects = [
       isTvClicked = true;
       const redArea = getAnotherElementById(e.target, "red-area");
       const monitor = getAnotherElementById(e.target, "monitor");
+      playAudio(process.env.PUBLIC_URL + '/Room0/scan.wav');
       showElementSoftly(redArea, 2000);
       showElementSoftly(monitor, 1000);
     },
   },
   {
-    imageUrl: process.env.PUBLIC_URL + '/Room0/layer-wall.png',
-    id: "1821",
-    x: 1030,
-    y: 575,
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall1",
+    x: 645,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall2",
+    x: 970,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall3",
+    x: 1290,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall4",
+    x: 1614,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall5",
+    x: 2255,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall6",
+    x: 2576,
+    y: 850,
+    isHover: false,
+    onClick: (e) => {
+      if (!isFakeDoorKnocked) {
+        isFakeDoorKnocked = true;
+        playAudio(process.env.PUBLIC_URL + '/Room0/bang2.mp3');
+      } else {
+        toast.info("اوه! بذار ببینم پشت اینجا چیه...");
+        playAudio(process.env.PUBLIC_URL + '/Room0/bang2.mp3');
+        setTimeout(() => {
+          window.location.href = '/room1';
+        }, 6000)
+      }
+    },
+  },
+  {
+    imageUrl: process.env.PUBLIC_URL + '/Room0/wall-layer-fake.png',
+    id: "wall7",
+    x: 2898,
+    y: 850,
     isHover: false,
     onClick: (e) => {
       playAudio(process.env.PUBLIC_URL + '/Room0/bang1.mp3');
