a89706de12ca156b136fd6ca7f4f7261f638a462
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-12-15 18:01:56 +0330

    add room1 media

---
+++ src/containers/Room1/index.js
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import useImage from 'use-image';
+
+import Scene from '../Scene';
+import Objects from './Objects';
+
+const useStyles = makeStyles(() => ({
+  fullHeight: {
+    minHeight: '100vh',
+  },
+}));
+
+const Index = () => {
+  const history = useHistory();
+  const classes = useStyles();
+
+  return (
+    <Scene backgroundImageLink={'/Room0/background.png'} objects={Objects} />
+  );
+};
+
+
+export default Index;
--- src/root/index.js
+++ src/root/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Room0 from '../containers/Room0';
+import Room1 from '../containers/Room1';
 
 
 const Root = () => {
   return (
     <Switch>
+      <Route path="/room1/" component={Room1} />
       <Route path="/room0/" component={Room0} />
+      <Route path="*" component={Room0} />
     </Switch>
   );
 };
